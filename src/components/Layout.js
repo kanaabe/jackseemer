7a663d205258e68a5b2f42eb39be54493b037a6c
Kanako Abe <kana@Kanakos-MacBook-Pro.local>
2018-10-30 15:32:18 -0400

    Strip to Coming Soon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 
-import Navbar from '../components/Navbar'
+// import Navbar from '../components/Navbar'
 import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
-    <Navbar />
+    <Helmet title="Jack Seemer" />
+    {/* <Navbar /> */}
     <div>{children}</div>
   </div>
 )
